64db4cf3055160420cf9136747809f9516746085
rob sullivan <rob@datachomp.com>
2015-11-07 20:22:31 -0600

    couple quick rubocop and critic updates

--- app/models/blog_roll.rb
+++ app/models/blog_roll.rb
@@ -5,8 +5,8 @@ class BlogRoll < ActiveRecord::Base
 
   def self.harvest_new_entries()
     rollin = BlogRoll.where('active_flag = true')
-    rollin.each do |r|
-      parse_feed_normal(r[:feed_uri], r[:id], Time.now - 3.days)
+    rollin.each do |roll|
+      parse_feed_normal(roll[:feed_uri], roll[:id], Time.now - 3.days)
     end
   end
 
@@ -19,7 +19,7 @@ class BlogRoll < ActiveRecord::Base
       roll.feed_type = feed_type
       roll.save
 
-      #back fill the last 3 months
+      # back fill the last 3 months
       parse_feed_normal(roll.feed_uri, roll.id, Time.now - 3.months)
   end
 
@@ -29,33 +29,21 @@ class BlogRoll < ActiveRecord::Base
 
     begin
         feedinfo = GoogleAjax::Feed.load(feed_uri)
-        feedinfo[:entries].each do |c|
+        feedinfo[:entries].each do |info|
 
-          if c[:published_date].to_date > datefilter
-            toppy = BlogEntry.where("entry_uri = ?", c[:link]).select(:id).first
+          if info[:published_date].to_date > datefilter
+            entry_exists = BlogEntry.where("entry_uri = ?", info[:link]).select(:id).first
 
-            if toppy.nil?
+            unless entry_exists
               entry = BlogEntry.new
               entry.blog_roll_id = blog_id
-              entry.title = c[:title]
-              entry.entry_uri = c[:link]
-              entry.posted_date = c[:published_date]
+              entry.title = info[:title]
+              entry.entry_uri = info[:link]
+              entry.posted_date = info[:published_date]
               entry.save
             end
-          end#end date check
-        end #end feed info loop
+          end # end date check
+        end # end feed info loop
     end
   end
-
 end
-=begin
-BlogRoll.create('Jordan Rousseau', 'Jordan Rousseau', 'http://rousseau.io/', 'http://rousseau.io/atom.xml', 'atom' )
-BlogRoll.create('Groove Coder', 'Luke Crouch', 'http://groovecoder.com/', 'http://groovecoder.com/feed/', 'feed' )
-Luke Crouch - http://groovecoder.com/feed/
-BlogRoll.create('Buddy Lindsey', 'Buddy Lindsey', 'http://buddylindsey.com/', 'http://buddylindsey.com/feed/', 'live' )
-
-
-BlogRoll.create('Devin Clark', 'Devin Clark', 'http://devin-clark.com/', 'http://devin-clark.com/rss/', 'rss' )
-BlogRoll.create('Jesse Harlin', 'Jesse Harlin', 'http://www.simiansblog.com/', 'http://www.simiansblog.com/rss?containerid=52', 'word' )
-http://devin-clark.com/rss/
-=end
