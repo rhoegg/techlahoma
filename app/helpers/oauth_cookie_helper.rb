c424ce1be60c0909b40e04a5fd798f00ec7c76e0
Jeremy Green <jeremy@octolabs.com>
2016-02-12 23:16:47 -0600

    Add some debugging

--- app/helpers/oauth_cookie_helper.rb
+++ app/helpers/oauth_cookie_helper.rb
@@ -2,12 +2,13 @@ module OauthCookieHelper
 
   def env_domain
     domain = ENV['COOKIE_DOMAIN'] || 'localhost'
-    Rails.logger.error("trying to set cookie for #{domain}")
     domain == "localhost" ? :all : domain
   end
 
   def set_user_cookie(user)
+    Rails.logger.error("trying to set cookie for #{env_domain}")
     cookies[:techlahoma_auth] = { :value => user.id, :domain => env_domain }
+    Rails.logger.error("cookie = #{cookies[:techlahoma_auth]}")
   end
 
   def remove_user_cookie
